1daff32f4f0d90837594d9b4795ef9bbf414fb91
Srikar Srivatsav V S <srikarsrivatsavvs@Srikars-MacBook-Air.local>
2023-06-24 22:06:49 +0530

    refactored code, email template modifications

--- send_email.py
+++ send_email.py
@@ -1,5 +1,5 @@
 import boto3
-import os
+import os, json
 from dotenv import load_dotenv
 
 def send_email(email, content):
@@ -10,6 +10,24 @@ def send_email(email, content):
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
         aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
     )
+    email_message = {
+    'Subject': {
+        'Data': 'Summary of the github repo pull requests you requested!'
+    },
+    'Body': {
+        'Text': {
+            'Data': 'Here\'s the latest 5 pull request this week with user\'s name and summary \n'
+            }
+        }
+    }
+    for i, pr in enumerate(content):
+        if i<5:
+            print(i, pr)
+            email_message['Body']['Text']['Data'] += f"\n Name : {pr['name']} ---- Title of the pr: {pr['title']}"
+        else:
+            break
+    email_json = json.dumps(email_message)
+
     response = ses_client.send_email(
     Source='srikarsrivatsavvs@gmail.com',
     Destination={
@@ -17,11 +35,14 @@ def send_email(email, content):
     },
     Message={
         'Subject': {
-            'Data': 'Summary of the github repo pull requests you requested!'
+            'Data': email_message['Subject']['Data']
         },
         'Body': {
             'Text': {
-                'Data': str(content)
+                'Data': f"{email_message['Body']['Text']['Data']} \
+                \n \n For the entire PR data of last week, please check the api response through Postman! \
+                \n \
+                \n Thanks!"
             }
         }
     }
